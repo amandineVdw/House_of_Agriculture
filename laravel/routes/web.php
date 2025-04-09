44d44b51ba8b7adf2e9111d8de7cb62d9f44f486
Juan Andres <j.imbaquingo@outlook.fr>
2025-04-09 16:34:05 +0200

    module course: creation de la structure de table et routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('courses', CourseController::class);
+});
 
 Route::get('/', function () {
     return view('welcome');
