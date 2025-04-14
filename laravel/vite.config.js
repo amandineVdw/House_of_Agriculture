41276369c8e2ed083c4ee753ca2fe4ca3fc358af
Amandine Van de Winckel <164058998+amandineVdw@users.noreply.github.com>
2025-04-14 12:43:13 +0200

    Fix Docker and Laravel configurations, clean up cache and view files
    - Added .dockerignore to exclude unnecessary files from Docker build.
    - Modified docker-compose.yml for service configurations.
    - Cleaned up log files and cache from previous builds.
    - Deleted old view and cache files from Laravel framework.
    - Updated Dockerfile and vite.config.js for Laravel app.

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',  // 👈 permet l'accès depuis ta machine hôte
+        port: 5173,       // 👈 optionnel, mais tu peux fixer le port si tu veux
+        strictPort: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
