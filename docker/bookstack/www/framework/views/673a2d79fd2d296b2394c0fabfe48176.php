676405592b94c7a14fe8ab10b3d4bfae4105f0e7
Juan Andres <j.imbaquingo@outlook.fr>
2025-04-17 00:32:31 +0200

    feat:configuration api en place

--- docker/bookstack/www/framework/views/673a2d79fd2d296b2394c0fabfe48176.php
+++
@@ -1,5 +0,0 @@
-
-
-<h1 class="list-heading"><?php echo e($title); ?></h1>
-<h5><?php echo e($subtitle); ?></h5>
-<p><?php echo e($details); ?></p><?php /**PATH /app/www/resources/views/errors/parts/not-found-text.blade.php ENDPATH**/ ?>
--- docker/bookstack/www/framework/views/f3de54f0302e0e9b5567716d860d30d6.php
+++
@@ -1,8 +0,0 @@
-<a href="<?php echo e($entity->getUrl()); ?>" class="flex-container-row items-center">
-    <span role="presentation"
-          class="icon flex-none text-<?php echo e($entity->getType()); ?>"><?php echo (new \BookStack\Util\SvgIcon($entity->getType()))->toHtml(); ?></span>
-    <div class="flex text-<?php echo e($entity->getType()); ?>">
-        <?php echo e($entity->name); ?>
-
-    </div>
-</a><?php /**PATH /app/www/resources/views/entities/icon-link.blade.php ENDPATH**/ ?>
