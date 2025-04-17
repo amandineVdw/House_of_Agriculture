66313de81b0941135179996e2df3a56a00ed89c3
Amandine Van de Winckel <164058998+amandineVdw@users.noreply.github.com>
2025-04-17 13:50:14 +0200

    feat: Merge branch 'develop' into feature/page-layout

--- laravel/resources/views/layouts/plain.blade.php
+++ laravel/resources/views/layouts/plain.blade.php
@@ -35,9 +35,6 @@
         @yield('content')
     </div>
 
-    <!-- ✅ Script JS Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 
 </html>
